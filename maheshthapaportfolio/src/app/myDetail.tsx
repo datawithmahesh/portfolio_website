95fdf791456de75682e3d949379b6f4065bfe156
datawithmahesh <maheshthapa3150@gmail.com>
2025-11-20 19:41:09 +0545

    new commit added

--- maheshthapaportfolio/src/app/myDetail.tsx
+++ maheshthapaportfolio/src/app/myDetail.tsx
@@ -34,7 +34,7 @@ export const contact: Contact = {
 
 
 
-export const resumeUrl = 'https://drive.google.com/file/d/1mMu8qKet6n7-Dj3bIv5BcRSitah00S-_/view?usp=sharing'; // place resume.pdf in /public or use external URL
+export const resumeUrl = 'https://drive.google.com/file/d/14L_C9AuLtL1S33lIKoLdcK7-Kt1gUpqa/view?usp=sharing'; // place resume.pdf in /public or use external URL
 
 // -------------------------
 // SEO
